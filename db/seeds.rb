32e9f689e7e2e3b8b525007cbd156b66ec408947
Matthew Cwalina <matthewcwalina@gmail.com>
2022-02-07 17:20:21 +0100

    Added some attributes to golf course

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ user1 = User.create!(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   age: rand(12..90),
-  handicap: rand(0..30)
-  email: "tomkiller@gmail.com"
-  password: 123456
+  handicap: rand(0..30),
+  email: "tomkiller@gmail.com",
+  password: 123456,
   address: "1511 Wildrose Drive, Garndernville, NV 89410, United States"
 )
 
@@ -24,9 +24,9 @@ user2 = User.create!(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   age: rand(12..90),
-  handicap: rand(0..30)
-  email: "threeput@gmail.com"
-  password: 123456
+  handicap: rand(0..30),
+  email: "threeput@gmail.com",
+  password: 123456,
   address: "8452 Sunkist Cirlce, Huntington Beach, CA 92646, United States"
 )
 
@@ -34,39 +34,39 @@ user3 = User.create!(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   age: rand(12..90),
-  handicap: rand(0..30)
-  email: "eaglebaby@gmail.com"
-  password: 123456
+  handicap: rand(0..30),
+  email: "eaglebaby@gmail.com",
+  password: 123456,
   address: "27 N 6th St, San Jose, CA 95112, United States"
 )
 
 # Adding Buddies to database
 friend1 = Buddy.create!(
   user: user1,
-  golf_course: 'Pebble Beach',
+  golf_course: 'Pebble Beach'
 )
 
 friend2 = Buddy.create!(
   user: user1,
-  golf_course: 'Spy Glass',
+  golf_course: 'Spy Glass'
 )
 
 friend3 = Buddy.create!(
   user: user1,
-  golf_course: 'Edgewood',
+  golf_course: 'Edgewood'
 )
 
 friend4 = Buddy.create!(
   user: user1,
-  golf_course: 'Coyote Moon',
+  golf_course: 'Coyote Moon'
 )
 
 friend5 = Buddy.create!(
   user: user1,
-  golf_course: 'Olympic Club',
+  golf_course: 'Olympic Club'
 )
 
 friend6 = Buddy.create!(
   user: user1,
-  golf_course: 'Riveria Country Club',
+  golf_course: 'Riveria Country Club'
 )
