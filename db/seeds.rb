0448e94feef42f3e0c37b911b430a23bd0a05537
Matthew Cwalina <matthewcwalina@gmail.com>
2022-02-10 15:11:55 +0100

    Seeds are created

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,38 +9,6 @@ GolfCourse.destroy_all
 
 puts 'Creating more users (phew)...'
 
-# Adding Users to database
-
-user1 = User.create!(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  age: rand(12..90),
-  handicap: rand(0..30),
-  email: "tomkiller@gmail.com",
-  password: 123456,
-  address: "1511 Wildrose Drive, Garndernville, NV 89410, United States"
-)
-
-user2 = User.create!(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  age: rand(12..90),
-  handicap: rand(0..30),
-  email: "threeput@gmail.com",
-  password: 123456,
-  address: "8452 Sunkist Cirlce, Huntington Beach, CA 92646, United States"
-)
-
-user3 = User.create!(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  age: rand(12..90),
-  handicap: rand(0..30),
-  email: "eaglebaby@gmail.com",
-  password: 123456,
-  address: "27 N 6th St, San Jose, CA 95112, United States"
-)
-
 #Adding Golf Courses to database
 
 pebble_beach = GolfCourse.create!(
@@ -78,6 +46,42 @@ riviera = GolfCourse.create!(
   name: "Riviera Country Club",
   par: 71,
 )
+
+# Adding Users to database
+
+user1 = User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  age: rand(12..90),
+  handicap: rand(0..30),
+  email: "tomkiller@gmail.com",
+  password: 123456,
+  address: "1511 Wildrose Drive, Garndernville, NV 89410, United States",
+  golf_course: edgewood
+)
+
+user2 = User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  age: rand(12..90),
+  handicap: rand(0..30),
+  email: "threeput@gmail.com",
+  password: 123456,
+  address: "8452 Sunkist Cirlce, Huntington Beach, CA 92646, United States",
+  golf_course: riviera
+)
+
+user3 = User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  age: rand(12..90),
+  handicap: rand(0..30),
+  email: "eaglebaby@gmail.com",
+  password: 123456,
+  address: "27 N 6th St, San Jose, CA 95112, United States",
+  golf_course: pebble_beach
+)
+
 # Adding Buddies to database
 
 friend1 = Buddy.create!(
