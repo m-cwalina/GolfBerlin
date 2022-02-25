b1b102d6252414b691bcd272d5715f384ffef6ab
Matthew Cwalina <matthewcwalina@gmail.com>
2022-02-25 13:51:50 +0100

    Created a functional searchbar with list

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { useState } from "react";
 import Data from "../data/mock_data.json";
 
 const App = () => {
+  const [query, setQuery] = useState("")
+
   return (
     <div>
-      <input placeholder="Enter a Buddies Name" />
+      <div className="left-scene">
+        <input placeholder="Enter a Buddies Name" onChange={event => setQuery(event.target.value)} />
         {
-          Data.map((buddy) => (
-            <div key={buddy.id}>
+          Data.filter(buddy => {
+            if (query === '') {
+              return buddy;
+            } else if (buddy.first_name.toLowerCase().includes(query.toLowerCase())) {
+              return buddy;
+            }
+          }).map((buddy) => (
+            <div className="box" key={buddy.id}>
               <h2>{buddy.first_name} {buddy.last_name}</h2>
               <p>{buddy.address}</p>
             </div>
           ))
         }
+      </div>
+      <div className="right-scene">
+      </div>
     </div>
   )
 }
+
 export default App;
