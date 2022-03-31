86211f9e25430c9f01b007a87a31081ce1c12230
Matthew Cwalina <matthewcwalina@gmail.com>
2022-03-31 16:20:03 +0200

    Created tiles for buddies page

--- app/javascript/components/SearchBar.js
+++ app/javascript/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 import './searchbar.css';
@@ -25,13 +25,13 @@ function SearchBar( { placeholder, data }) {
     <div className="search">
       <div className="searchInput">
         <input type="text" placeholder={placeholder} value={wordEntered} onChange={handleFilter}/>
-        <div className="searchIcon">
-        {filteredData.length === 0 ? <SearchIcon /> : <CloseIcon id="clearBtn" onClick={clearInput} />}
+          <div className="searchIcon">
+          {filteredData.length === 0 ? <SearchIcon /> : <CloseIcon id="clearBtn" onClick={clearInput} />}
+          </div>
         </div>
-        </div>
-      <div className="dataResult">
+      <div className="tiles">
         {filteredData.slice(0, 15).map((value, key) => {
-          return <a className="dataItem"><p>{value.first_name} {value.last_name}</p></a>
+          return <a className="tile"><p>{value.first_name} {value.last_name}</p></a>
         })}
       </div>
     </div>
