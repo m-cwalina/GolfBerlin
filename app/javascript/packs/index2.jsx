3d153653409cd12be8ec25ce97dbd46a360919e8
Matthew Cwalina <matthewcwalina@gmail.com>
2022-04-04 16:37:41 +0200

    Created a new react application for golf courses

---
+++ app/javascript/packs/index2.jsx
@@ -0,0 +1,12 @@
+// external modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app2';
+
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
