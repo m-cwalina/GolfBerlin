035cad21f9d0e9d9626f8a2da279bca5b54f578a
Matthew Cwalina <matthewcwalina@gmail.com>
2022-04-05 13:50:07 +0200

    Refactored App.jsx and SearchBar.jsx

--- app/javascript/components/SearchBar.jsx
+++ app/javascript/components/SearchBar.jsx
@@ -1,41 +1,18 @@
-import React, {useState} from 'react';
-import Buddies from './Buddies'
+import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 import './searchbar.css';
 
-function SearchBar({placeholder, data}) {
-  const [filteredData, setFilteredData] = useState([]);
-  const [wordEntered, setWordEntered] = useState("");
-
-  /*This handles the filtering function. Takes data from input and filters array of buddies*/
-
-  const handleFilter = (event) => {
-    const searchWord = event.target.value
-    setWordEntered(searchWord);
-    const newFilter = data.filter((value) => {
-      return value.first_name.toLowerCase().includes(searchWord.toLowerCase());;
-    });
-    setFilteredData(newFilter);
-  }
-
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
-  }
-
+function SearchBar({placeholder, handleFilter, wordEntered, clearInput, buddies}) {
   return (
     <div className="search">
       <div className="searchArea">
         <div className="searchBar">
           <input type="text" placeholder={placeholder} value={wordEntered} onChange={handleFilter}/>
           <div className="searchIcon">
-            {filteredData.length === 0 ? <SearchIcon /> : <CloseIcon id="clearBtn" onClick={clearInput} />}
+            {buddies.length === 0 ? <SearchIcon /> : <CloseIcon id="clearBtn" onClick={clearInput} />}
           </div>
         </div>
-      </div>
-      <div>
-        <Buddies filteredData={filteredData}/>
       </div>
     </div>
   )
--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,9 +1,30 @@
 import SearchBar from '../components/SearchBar';
+import Buddies from '../components/Buddies'
 import React, {useEffect, useState} from 'react';
 
 export default function App() {
   const [buddies, setBuddies] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  /*This handles the filtering function. Takes data from input and filters array of buddies*/
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value
+    setWordEntered(searchWord);
+    const newFilter = buddies.filter((value) => {
+      return value.first_name.toLowerCase().includes(searchWord.toLowerCase());;
+    });
+    setFilteredData(newFilter);
+  }
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  }
+
   /*Here is my API that connects to the API I created in Rails for my buddies*/
+
   const Api = async () => {
       const URL = "api/v1/buddies/index";
       try {
@@ -15,13 +36,17 @@ export default function App() {
       }
     };
   /*I use this function so the array of buddies will show upon screen loading*/
+
   useEffect(() => {
     Api();
   }, []);
 
   return (
     <div className="App">
-      <SearchBar placeholder="Find your buddy" data={buddies}/>
+      <SearchBar placeholder="Find your buddy" buddies={filteredData} handleFilter={handleFilter} wordEntered = {wordEntered} clearInput={clearInput}/>
+      <div className="Buddy-List">
+        <Buddies filteredData={filteredData} />
+      </div>
     </div>
   )
 }
