50f15578199486cd64b8f932d210ffc7a2833e79
Matthew Cwalina <matthewcwalina@gmail.com>
2022-04-06 15:58:13 +0200

    Updated Seeds with geolocations

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,36 +15,48 @@ pebble_beach = GolfCourse.create!(
   location: "Pebble Beach Resorts, 17-Mile Drive, Pebble Beach, CA 93953",
   name: "Pebble Beach",
   par: 72,
+  latitude: 36.5725,
+  longitude: 121.9486
 )
 
 spy_glass = GolfCourse.create!(
   location: "3206 Stevenson Dr, Pebble Beach, CA 93953, USA",
   name: "Spy Glass",
   par: 72,
+  latitude: 36.5824,
+  longitude: 121.9583
 )
 
 edgewood = GolfCourse.create!(
   location: "180 Lake Pkwy, Stateline, NV 89449, USA",
   name: "Edgewood Tahoe",
   par: 72,
+  latitude: 38.9662,
+  longitude: 119.9482
 )
 
 coyote_moon = GolfCourse.create!(
   location: "10685 Northwoods Blvd, Truckee, CA 96161, USA",
   name: "Coyote Moon",
   par: 71,
+  latitude: 39.3333,
+  longitude: 120.2112
 )
 
 olympic_club = GolfCourse.create!(
   location: "599 Skyline Blvd., San Francisco, CA 94132",
   name: "Olympic CLub",
   par: 72,
+  latitude: 37.7086,
+  longitude: 122.4933
 )
 
 riviera = GolfCourse.create!(
   location: "1250 Capri Dr, Pacific Palisades, CA 90272, USA",
   name: "Riviera Country Club",
   par: 71,
+  latitude: 26.1155,
+  longitude: 81.7357
 )
 
 # Adding Users to database
