5ed0f3355f4551ab08b742ef2cfcb86fb9a7b6a2
Matthew Cwalina <matthewcwalina@gmail.com>
2022-04-08 20:20:07 +0200

    Added center to google maps

--- app/javascript/components/Map.jsx
+++ app/javascript/components/Map.jsx
@@ -3,10 +3,10 @@ import GoogleMapReact from 'google-map-react';
 import Marker from './Marker'
 
 export default function Map(props) {
-  const defaultProps = {
+  const center = {
     center: {
-      lat: 37.7749,
-      lng: -122.4194,
+      lat: props.selectedCourse.latitude,
+      lng: props.selectedCourse.longitude,
     },
     zoom: 11
   };
@@ -16,8 +16,8 @@ export default function Map(props) {
     <div className="map" style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.API_KEY }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+        center={center.center}
+        zoom={center.zoom}
       >
         <Marker lat={props.selectedCourse.latitude} lng={props.selectedCourse.longitude} />
       </GoogleMapReact>
