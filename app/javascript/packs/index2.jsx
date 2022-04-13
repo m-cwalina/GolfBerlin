1043614473f48e9e4c5d759e963c65a430f2b855
Matthew Cwalina <matthewcwalina@gmail.com>
2022-04-13 18:36:43 +0200

    updated application.js

--- app/javascript/packs/index2.jsx
+++ app/javascript/packs/index2.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app2';
-import './index.css';
+import './index.scss';
 // render an instance of the component in the DOM
 ReactDOM.render(
   <React.StrictMode>
