92f4fff2c95d255f27dc37c178754cb428f98183
Matthew Cwalina <matthewcwalina@gmail.com>
2022-04-13 19:53:56 +0200

    App working without css when deployed with Heroku

--- app/javascript/components/Buddies.jsx
+++ app/javascript/components/Buddies.jsx
@@ -7,12 +7,11 @@ export default function Buddies(props) {
   const renderList = () => {
     return props.buddies.map((buddy) => {
       return (
-        <div className="buddy-tiles">
+        <div key={buddy.address} className="buddy-tiles">
           <div
             onMouseEnter={() => setHover(buddy.address)}
             onMouseLeave={() => setHover("")}
             className={hover === buddy.address ? "tile-border buddy-tile" : "buddy-tile"}
-            key={buddy.address}
           >
             <div><Buddy buddy={buddy} extraDetails={hover === buddy.address}/></div>
         </div>
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,8 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import '/Users/matthewcwalina/code/m-cwalina/GolfBerlin/app/assets/stylesheets/react.scss';
-
+import './react.scss';
 // render an instance of the component in the DOM
 ReactDOM.render(
   <React.StrictMode>
